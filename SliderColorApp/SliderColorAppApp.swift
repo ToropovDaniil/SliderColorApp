fd06ca4892cea4650b2e0f9e028583afe80255a3
ToropovDaniil <toropov.danii723@yandex.ru>
2023-12-17 19:00:58 +0300

    Initial Commit

---
+++ SliderColorApp/SliderColorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderColorAppApp.swift
+//  SliderColorApp
+//
+//  Created by Даниил Торопов on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderColorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
